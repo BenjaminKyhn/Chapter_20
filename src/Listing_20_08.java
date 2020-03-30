462e893bb60ce6d9f1aa5c9dbb0991fe44f1a95c
Benjamin Kyhn <benkyhn@hotmail.com>
2020-03-30 11:26:08 +0200

    Added listing 20.8

--- src/Listing_20_08.java
+++ src/Listing_20_08.java
@@ -4,7 +4,8 @@ import java.util.List;
 public class Listing_20_08 {
     public static void main(String[] args) {
         List<String> cities = Arrays.asList("Atlanta", "Savannah", "New York", "Dallas");
-        cities.sort((s1, s2) -> s1.compareToIgnoreCase(s2));
+        cities.sort((s1, s2) -> s1.compareToIgnoreCase(s2)); // The lambda expression creates a new comparator object which is equivalent to creating an anonymous inner class
+        // or simply use the method reference: cities.sort(String::compareToIgnoreCase);
 
         for (String s :
                 cities) {
